fd734a53f599f3f3f66692292416c6b19e5bf2bf
Dinko Georgiev <dinko359@gmail.com>
2026-01-20 11:42:53 +0200

    Prim Set Request and L12 support

---
+++ src/Requests/PrimSetRequest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Gdinko\Prim\Requests;
+
+use Gdinko\Prim\Exceptions\PrimValidationException;
+use Gdinko\Prim\Interfaces\PrimRequestInterface;
+use Illuminate\Support\Facades\Validator;
+
+class PrimSetRequest implements PrimRequestInterface
+{
+    /**
+     * validationRules
+     *
+     * @var array
+     */
+    protected $validationRules = [];
+
+    /**
+     * validatedData
+     *
+     * @var array
+     */
+    protected $validatedData = [];
+
+    /**
+     * __construct
+     *
+     * @param  array $data
+     * @param  bool $getAll
+     * @param  array $validationRules
+     * @param  bool $wrap
+     * @return void
+     */
+    public function __construct(array $data = [], array $validationRules = [], $wrap = false)
+    {
+        $this->validationRules = $validationRules;
+
+        $validator = Validator::make(
+            $data,
+            $this->getValidationRules()
+        );
+
+        if ($validator->fails()) {
+            throw new PrimValidationException(
+                __CLASS__,
+                422,
+                $validator->messages()->toArray()
+            );
+        }
+
+        $this->validatedData = [
+            'data' => $this->wrap(
+                $validator->validated() ?: ($data ?: new \stdClass()),
+                $wrap
+            ),
+        ];
+    }
+
+    /**
+     * getValidationRules
+     *
+     * @return array
+     */
+    public function getValidationRules(): array
+    {
+        if (! empty($this->validationRules)) {
+            return $this->validationRules;
+        }
+
+        return [];
+    }
+
+    /**
+     * getValidatedData
+     *
+     * @return array
+     */
+    public function getValidatedData(): array
+    {
+        return $this->validatedData;
+    }
+
+    /**
+     * wrap
+     *
+     * @param  mixed $data
+     * @param  bool $wrap
+     *
+     * @return array
+     */
+    protected function wrap($data, $wrap): array
+    {
+        if ($wrap && is_array($data)) {
+            return [$data];
+        }
+
+        if ($data instanceof \stdClass) {
+            return [$data];
+        }
+
+        return $data;
+    }
+}
